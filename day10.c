eb6c67d6f63b8a0e94b84987ffd262d5e9ea5ca6
Avni <thapliyal.avni2006@gmail.com>
2026-02-10 17:10:13 +0530

    Add files via upload

---
+++ day10.c
@@ -0,0 +1,36 @@
+/*Read a string and check if it is a palindrome using two-pointer comparison.
+
+Input:
+- Single line: string s
+
+Output:
+- Print YES if palindrome, otherwise NO*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char s[1000];
+    scanf("%s", s);
+    
+    int left = 0;
+    int right = strlen(s) - 1;
+    int isPalindrome = 1;
+    
+    while(left < right){
+        if(s[left] != s[right]){
+            isPalindrome = 0;
+            break;
+        }
+        left++;
+        right--;
+    }
+    
+    if(isPalindrome){
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+    
+    return 0;
+}
