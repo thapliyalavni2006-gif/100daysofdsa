9c2199c633113656e95802e654b6a2446a55a10c
Avni <thapliyal.avni2006@gmail.com>
2026-02-13 12:15:59 +0530

    Add files via upload

---
+++ day13.c
@@ -0,0 +1,56 @@
+/*You are given a rectangular matrix of integers. Starting from the outer boundary, traverse the matrix in a clockwise manner and continue moving inward layer by layer until all elements are visited.
+
+Input:
+- First line: two integers r and c representing the number of rows and columns
+- Next r lines: c integers each representing the matrix elements
+
+Output:
+- Print all visited elements in the order of traversal, separated by spaces*/
+
+#include <stdio.h>
+
+int main(){
+    int r, c;
+    scanf("%d %d", &r, &c);
+    
+    int matrix[r][c];
+    
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    
+    int top = 0, bottom = r - 1;
+    int left = 0, right = c - 1;
+    
+    while(top <= bottom && left <= right){
+        for(int i = left; i <= right; i++){
+            printf("%d ", matrix[top][i]);
+        }
+        top++;
+        
+        for(int i = top; i <= bottom; i++){
+            printf("%d ", matrix[i][right]);
+        }
+        right--;
+        
+        if(top <= bottom){
+            for(int i = right; i >= left; i--){
+                printf("%d ", matrix[bottom][i]);
+            }
+            bottom--;
+        }
+        
+        if(left <= right){
+            for(int i = bottom; i >= top; i--){
+                printf("%d ", matrix[i][left]);
+            }
+            left++;
+        }
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
