2478f9eeb4aa0d5bb8675d765fd889e3e154547d
Avni <thapliyal.avni2006@gmail.com>
2026-03-30 10:47:15 +0530

    Add files via upload

---
+++ day58.c
@@ -0,0 +1,20 @@
+/*Construct a binary tree from given preorder and inorder traversal arrays.
+
+Input Format:
+- First line contains integer N
+- Second line contains preorder traversal
+- Third line contains inorder traversal
+
+Output Format:
+- Print postorder traversal of constructed tree
+
+Example:
+Input:
+5
+1 2 4 5 3
+4 2 5 1 3
+
+Output:
+4 5 2 3 1
+*/
+
